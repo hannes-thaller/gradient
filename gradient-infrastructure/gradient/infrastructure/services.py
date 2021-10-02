28d339539523b567cfff3b4adfb515d2b0bcfdc1
Hannes Thaller <hannes.thaller.at@gmail.com>
2021-10-02 19:11:48 +0200

    Combine the infrastructure services.
    Revise the infrastructure task.
    Revise the buildspec caches

--- gradient-infrastructure/gradient/infrastructure/services.py
+++ gradient-infrastructure/gradient/infrastructure/services.py
@@ -1,7 +1,14 @@
+import logging
+import pathlib
+import subprocess
+
 import attr
+import requests
 import yaml
 from aws_cdk import core
 from . import components
+
+logger = logging.getLogger(__name__)
 
 
 @attr.s(frozen=True)
@@ -18,3 +25,36 @@ class InfrastructureService:
     def _load_buildspec(self, path: str):
         with open(path, "r") as f:
             return yaml.safe_load(f)
+
+    @staticmethod
+    def download_conda(url_conda: str) -> pathlib.Path:
+        assert url_conda
+
+        logger.info("Downloading conda installer")
+
+        path_installer = pathlib.Path("miniconda.sh")
+        response = requests.get(url_conda)
+        with path_installer.open("wb") as f:
+            f.write(response.content)
+
+        logger.info("Done downloading conda install")
+
+        return path_installer
+
+    @staticmethod
+    def install_conda(path_installer: pathlib.Path, dir_install: pathlib.Path):
+        assert path_installer.exists()
+
+        logger.info(f"Installing conda from {path_installer} into {dir_install}")
+
+        dir_install = pathlib.Path(dir_install)
+        dir_install.mkdir(parents=True, exist_ok=True)
+
+        subprocess.run(["sh", "path_installer", "-b", "-p", dir_install.absolute()])
+        subprocess.run(["source", dir_install.joinpath("etc", "profile.d", "conda.sh")])
+        subprocess.run(["conda", "config", "--set", "always_yes", "yes", "--set", "changeps1", "no"])
+        subprocess.run(["conda", "update", "-q", "conda"])
+        subprocess.run(["conda", "info", "-a"])
+        subprocess.run(["conda", "install", "-c", "conda-forge", "python=3.7", "invoke=1.5.0"])
+
+        logger.info(f"Done installing conda")
--- gradient-infrastructure/tasks.py
+++ gradient-infrastructure/tasks.py
@@ -1,11 +1,8 @@
 import logging
-import os.path
 import pathlib
 import shutil
-import subprocess
 from concurrent import futures
 
-import requests
 from invoke import task
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
@@ -28,10 +25,13 @@ def install(c):
 
 @task
 def install_conda(c, force=False):
+    from gradient.infrastructure import services
+
     logger.info("Installing conda")
+
     dir_conda = pathlib.Path(c.config.buildspec.dir_conda)
     if force or not dir_conda.exists():
-        service = BuildPipelineService()
+        service = services.InfrastructureService()
         path_installer = service.download_conda(c.config.buildspec.url_conda)
         service.install_conda(path_installer, dir_conda)
 
@@ -60,15 +60,11 @@ def test(c):
 
 
 @task
-def clean(c):
-    shutil.rmtree("cdk.out", ignore_errors=True)
-    c.run(f"aws ecr delete-repository")
-
-
-@task
-def remove_ecr_repos(c, force=False):
+def clean(c, force=False):
     import boto3
     from botocore import exceptions
+
+    shutil.rmtree("cdk.out", ignore_errors=True)
 
     names_repo = [it["ecr"]["name"] for repo in c.config["repo-stacks"]
                   for it in repo["modules"]]
@@ -88,16 +84,6 @@ def remove_ecr_repos(c, force=False):
 
 @task(pre=[clean, build])
 def publish(c):
-    logger.info("Publishing")
-
-    c.run(f"python setup.py sdist bdist_wheel")
-    logger.info(f"Generated the package")
-
-    logger.info("Done publishing")
-
-
-@task
-def publish_infrastructure(c):
     logger.info("Publishing Infrastructure")
 
     c.run(f"cdk deploy --all")
@@ -108,38 +94,3 @@ def publish_infrastructure(c):
 @task
 def run(c):
     pass
-
-
-class BuildPipelineService:
-    @staticmethod
-    def download_conda(url_conda: str) -> pathlib.Path:
-        assert url_conda
-
-        logger.info("Downloading conda installer")
-
-        path_installer = pathlib.Path("miniconda.sh")
-        response = requests.get(url_conda)
-        with path_installer.open("wb") as f:
-            f.write(response.content)
-
-        logger.info("Done downloading conda install")
-
-        return path_installer
-
-    @staticmethod
-    def install_conda(path_installer: pathlib.Path, dir_install: pathlib.Path):
-        assert os.path.exists(path_installer)
-
-        logger.info(f"Installing conda from {path_installer} into {dir_install}")
-
-        dir_install = pathlib.Path(dir_install)
-        dir_install.mkdir(parents=True, exist_ok=True)
-
-        subprocess.run(["sh", "path_installer", "-b", "-p", dir_install.absolute()])
-        subprocess.run(["source", dir_install.joinpath("etc", "profile.d", "conda.sh")])
-        subprocess.run(["conda", "config", "--set", "always_yes", "yes", "--set", "changeps1", "no"])
-        subprocess.run(["conda", "update", "-q", "conda"])
-        subprocess.run(["conda", "info", "-a"])
-        subprocess.run(["conda", "install", "-c", "conda-forge", "python=3.7", "invoke=1.5.0"])
-
-        logger.info(f"Done installing conda")
