ae2f00a72fe89e4bbe2a50cc98ee95e2c9c21215
Hannes Thaller <hannes.thaller.at@gmail.com>
2021-10-09 09:41:53 +0200

    Reivse the configuration structure and add tech stack proeprties.
    Add code artifact configuration.

--- gradient-infrastructure/gradient/infrastructure/components.py
+++ gradient-infrastructure/gradient/infrastructure/components.py
@@ -1,12 +1,40 @@
 import typing
 
 import constructs
-from aws_cdk import core, aws_codebuild, aws_ecr, aws_s3
+from aws_cdk import core, aws_codebuild, aws_ecr, aws_s3, aws_codeartifact, aws_iam
 
 
 class InfrastructureStack(core.Stack):
-    def __init__(self, scope: constructs.Construct, id: str = "gradient-infrastructure", **kwargs):
+    def __init__(self, scope: constructs.Construct, config: dict, id: str = "gradient-infrastructure", **kwargs):
         super().__init__(scope, id, **kwargs)
+
+        domain = self.create_codeartifact_domain(config["app"]["code-artifact"]["name_domain"])
+
+        for config_tech in config["app"]["tech-stacks"]:
+            config_ca = config_tech["code-artifact"]
+            repo = self.create_codeartifact_repo(
+                name=config_ca["name"],
+                description=config_ca["description"],
+                domain=domain,
+                account=config["app"]["account"],
+                external_connections=config_ca["external_connections"]
+            )
+
+    def create_codeartifact_domain(self, name):
+        return aws_codeartifact.CfnDomain(scope=self,
+                                          id="gradient-codeartifact-domain",
+                                          domain_name=name)
+
+    def create_codeartifact_repo(self, name, description, domain, account, external_connections):
+        repo = aws_codeartifact.CfnRepository(scope=self,
+                                              id=f"codeartifact-repository-{name}",
+                                              domain_name=domain.domain_name,
+                                              domain_owner=account,
+                                              external_connections=external_connections,
+                                              repository_name=name,
+                                              description=description)
+        repo.add_depends_on(domain)
+        return repo
 
 
 class BuildStack(core.NestedStack):
@@ -16,9 +44,10 @@ class BuildStack(core.NestedStack):
         config_cb = config["code-build"]
 
         env = self.create_build_env(config_cb["build_image"])
-        filters = self.create_filter_groups(config_cb["name"])
+        filters = self.create_filter_groups(config_cb["location_module"])
         source = self.create_source(config_cb["owner"], config_cb["repo"], filters)
         cache = self.create_cache_bucket()
+        role = self.create_code_build_role(config_cb["name"])
         project = self.create_code_build_project(config_cb["name"], config_cb["description"], env, source, build_spec, cache)
 
         for config_module in config["modules"]:
@@ -57,7 +86,38 @@ class BuildStack(core.NestedStack):
             webhook_filters=filters
         )
 
+    def create_code_build_role(self, name):
+        statement = aws_iam.PolicyStatement(
+            effect=aws_iam.Effect.ALLOW,
+            actions=[
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeImages",
+                "ecr:GetAuthorizationToken",
+                "ecr:InitiateLayerUpload",
+                "ecr:ListImages",
+                "ecr:PutImage",
+                "ecr:UploadLayerPart",
+                "logs:*",
+            ],
+            resources=["*"]
+        )
+
+        document = aws_iam.PolicyDocument(statements=[statement])
+
+        return aws_iam.Role(
+            scope=self,
+            id=f"codebuild-role",
+            role_name=f"codebuild-role-{name}",
+            path="/",
+            assumed_by=aws_iam.ServicePrincipal("codebuild.amazonaws.com"),
+            inline_policies={
+                "CodeBuildAccess": document
+            }
+        )
+
     def create_code_build_project(self, name, description, env, source, buildspec, cache):
+
         project = aws_codebuild.Project(
             self,
             "code-build",
@@ -68,7 +128,8 @@ class BuildStack(core.NestedStack):
             timeout=core.Duration.minutes(10),
             source=source,
             build_spec=aws_codebuild.BuildSpec.from_object(buildspec),
-            cache=aws_codebuild.Cache.bucket(cache)
+            cache=aws_codebuild.Cache.bucket(cache),
+
         )
         cache.grant_read_write(project)
         return project
--- gradient-infrastructure/gradient/infrastructure/services.py
+++ gradient-infrastructure/gradient/infrastructure/services.py
@@ -2,7 +2,7 @@ import logging
 
 import attr
 import yaml
-from aws_cdk import core
+from aws_cdk import core, aws_iam
 
 from . import components
 
@@ -13,12 +13,20 @@ logger = logging.getLogger(__name__)
 class InfrastructureService:
     def create_infrastructure(self, config):
         app = core.App()
-        stack_infrastructure = components.InfrastructureStack(app)
-        for it in config["repo-stacks"]:
-            build_spec = self._load_buildspec(it["code-build"]["build_spec"])
-            stacks_build = components.BuildStack(stack_infrastructure, it["id"], it, build_spec)
+
+        stack_infrastructure = components.InfrastructureStack(app, config)
+        self.create_module_stacks(stack_infrastructure, config)
+
         app.synth()
         return app
+
+    def create_module_stacks(self, stack_parent, config):
+        for it in config["repo-stacks"]:
+            build_spec = self._load_buildspec(it["code-build"]["build_spec"])
+            stacks_build = components.BuildStack(scope=stack_parent,
+                                                 id=it["id"],
+                                                 config=it,
+                                                 build_spec=build_spec)
 
     def _load_buildspec(self, path: str):
         with open(path, "r") as f:
